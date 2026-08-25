3cb24cac26aca81bdb6b65317a5181757d4a5a8e
pranavreddy1721 <pranavreddy1721@gmail.com>
2026-08-25 13:29:32 +0530

    refactor: move GraphQL schema to graphql file

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,70 +1,13 @@
 import { createSchema } from "graphql-yoga";
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+
 import { resolvers } from "./resolvers.ts";
 
-const typeDefs = /* GraphQL */ `
-  type Folder {
-    id: ID!
-    name: String!
-    createdAt: String!
-    updatedAt: String!
-    bookmarks: [Bookmark!]!
-  }
-
-  type Bookmark {
-    id: ID!
-    title: String!
-    url: String!
-    tags: [String!]!
-    folderId: ID!
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type BookmarkPage {
-    items: [Bookmark!]!
-    nextCursor: String
-    hasNextPage: Boolean!
-  }
-
-  type Query {
-    folders: [Folder!]!
-    folder(id: ID!): Folder
-    bookmarks(
-      first: Int!
-      after: String
-      folderId: ID
-      search: String
-    ): BookmarkPage!
-  }
-
-  type Mutation {
-    createFolder(name: String!): Folder!
-    updateFolder(id: ID!, name: String!): Folder!
-    deleteFolder(id: ID!): Boolean!
-
-    createBookmark(
-      title: String!
-      url: String!
-      tags: [String!]!
-      folderId: ID!
-    ): Bookmark!
-
-    updateBookmark(
-      id: ID!
-      title: String!
-      url: String!
-      tags: [String!]!
-      folderId: ID!
-    ): Bookmark!
-
-    deleteBookmark(id: ID!): Boolean!
-
-    moveBookmark(
-      id: ID!
-      folderId: ID!
-    ): Bookmark!
-  }
-`;
+const typeDefs = readFileSync(
+  fileURLToPath(new URL("./schema.graphql", import.meta.url)),
+  "utf8",
+);
 
 export const schema = createSchema({
   typeDefs,
